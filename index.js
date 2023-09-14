8328158373839cd673ac5b2d932354d8442f8b7f
Rafaell Victor Christian <rafaellvictor3@gmail.com>
2023-09-14 20:55:20 +0700

    make todo list app

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1. Reference HTML element in js
+const formTask = document.querySelector(".form-task");
+
+// 2. Menambahkan event listener di element form
+formTask.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // 3. Reference HTML element input
+  const inputForm = document.querySelector(".input-form");
+  console.log(inputForm.value);
+
+  // 4. Reference HTML element ul / wapper list
+  const wrapperList = document.querySelector(".task-list-wrapper");
+
+  // 5. Create <li> using JS
+  const taskList = document.createElement("li");
+
+  // 6. Masukkan input value ke <li>
+  taskList.innerHTML = inputForm.value;
+
+  // 7. Append <li> ke <ul>
+  wrapperList.append(taskList);
+});
